637b199b1609ae3b93ce838274b7159500a692fb
Dominik Stodolny <d.stodolny@gmail.com>
2015-01-23 23:54:09 +0100

    finish Word class

--- lib/hangman/word.rb
+++ lib/hangman/word.rb
@@ -1,22 +1,31 @@
 module Hangman
   class Word
-    attr_reader :result
+    attr_reader :result, :incorrect_letters
 
     def initialize
       @word_to_guess = random_word
       @result = '_' * @word_to_guess.size
+      @incorrect_letters = []
     end
 
-    def contain?(letter)
-      @word_to_guess.include?(letter)
+    def play(letter)
+      @word_to_guess.include?(letter) ? add_to_result(letter) : add_to_incorrect_letters(letter)
+      feedback
+    end
+
+    private
+
+    def feedback
+      "#{result.split('').join(' ')}\tincorrect: #{incorrect_letters.sort.join(', ')}"
     end
 
     def add_to_result(letter)
       @word_to_guess.split('').each_with_index { |e, i| result[i] = letter if e == letter }
-      result
     end
 
-    private
+    def add_to_incorrect_letters(letter)
+      incorrect_letters.push(letter)
+    end
 
     def random_word
       dictionary = File.read('./5desk.txt').split
